0f86fe154fc6b87a38e302b672e7624ca3a81f18
Risha <rishallen@gmail.com>
2016-08-08 12:04:09 -0700

    Add functions to main.js

--- main.js
+++ main.js
@@ -17,4 +17,30 @@ var main = function(){
   console.log('Hello,', gmail.get.user_email());
 };
 
+
+//Returns the text of the first opened composed email:
+var returns_text = function(){
+  gmail = new Gmail(); // instantiate
+  var email_id = gmail.get.compose_ids()[0];
+  var email_blob = gmail.get.email_data(email_id);
+  return email_blob.threads[email_id].content_plain;
+};
+
+
+//Returns the html element (button) of the first composed email
+var make_button = function(compose, type) {
+  gmail = new Gmail();
+  var compose_ref = gmail.dom.composes()[0];
+  gmail.tools.add_compose_button(compose_ref, '<div class="T-I J-J5-Ji T-I-KE L3" role="button" tabindex="0" gh="cm" style="-webkit-user-select: none;">ANALYZE</div>', function() {
+  }, 'Custom Style Classes');
+};
+
+
+//Executes when the Document Object Model (DOM) is ready for JavaScript code
+$( document ).ready(function() {
+  gmail = new Gmail();
+  gmail.observe.on("compose", make_button);
+  console.log( "ready!" );
+});
+
 refresh(main);

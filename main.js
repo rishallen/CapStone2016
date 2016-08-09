e0fe228258e4f0209256e3050ab37dd26a2e6ee1
Risha <rishallen@gmail.com>
2016-08-09 16:07:42 -0700

    moved code around to use the main function in file.

--- main.js
+++ main.js
@@ -11,30 +11,33 @@ function refresh(f) {
 
 
 var main = function() {
-  // NOTE: Always use the latest version of gmail.js from
-  // https://github.com/KartikTalwar/gmail.js
   gmail = new Gmail();
+  gmail.observe.on(
+    "compose", make_button
+  );
+    console.log( 'ready!' );
 };
 
+
+//Returns the text of the first opened composed email:
+var returns_text = function(){
+  gmail = new Gmail(); // instantiate
+  var email_id = gmail.get.compose_ids()[0];
+  var email_blob = gmail.get.email_data(email_id);
+  return email_blob.threads[email_id].content_plain;
+};
 
 //Returns the html element (button) of the last composed email
 var make_button = function(compose, type) {
   gmail = new Gmail();
   var compose_ref = gmail.dom.composes();
-  gmail.tools.add_compose_button(compose_ref[compose_ref.length - 1], 'Analyze', function() {
-    alert('you have invaded me');
-  }, 'emailBlobButton' ); // using the "class"
+  gmail.tools.add_compose_button(
+  compose_ref[compose_ref.length - 1], 'Analyze', function() {
+  var email_id = gmail.get.compose_ids()[0];
+  var email_blob = gmail.get.email_data(email_id);
+  var email_text = email_blob.threads[email_id].content_plain; // this needs be sent to api
+  }, 'btn-primary' ); // using the "class"
 };
 
 
-//Executes when the Document Object Model (DOM) is ready for JavaScript code
-$( document ).ready(function() {
-  gmail = new Gmail();
-
-    gmail.observe.on(
-      "compose", make_button
-    );
-      console.log( 'ready!' );
-});
-
-// refresh(main);
+refresh(main);
